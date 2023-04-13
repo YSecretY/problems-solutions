a52199fa35efda395893599e5eff5cae7dc75389
Bogdan Hrachov <bogdanhrachov@gmail.com>
2023-04-13 12:07:58 +0300

    Create 100. Same Tree.go

---
+++ LeetCode/100. Same Tree.go
@@ -0,0 +1,9 @@
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    if p == nil && q == nil {
+        return true
+    }
+    if p == nil || q == nil {
+        return false
+    }
+    return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
